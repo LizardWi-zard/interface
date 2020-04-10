1b0ecaacb26c38470b9c9cb66e17a8f628f0e2de
Lizard_W1zard <artemef1996@gmail.com>
2020-04-11 01:04:40 +0300

    /

--- interface/Program.cs
+++ interface/Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
 
-namespace Interface
+namespace HelloApp
 {
+    interface IShooting
+    {
+        void Shoot(string messege);
+    }
+
+    class Tank : IShooting
+    {
+        public void Shoot(string messege)
+        {
+            Console.WriteLine(messege);
+        }
+    }
+
+    class Halicopter : IShooting
+    {
+        public void Shoot(string messege)
+        {
+            Console.WriteLine(messege);
+        }
+    }
+
+    class Rifle : IShooting
+    {
+        public void Shoot(string messege)
+        {
+            Console.WriteLine(messege);
+        }
+    }
+
+
     class Program
     {
+        static void Action(IShooting shooter, string messege)
+        {
+            shooter.Shoot(messege);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Tank tank = new Tank();
+            Halicopter halicopter = new Halicopter();
+            Rifle rifle = new Rifle();
+
+            Action(tank, "Выстрелил фугасом из орудия");
+            Action(halicopter, "Дал несколько пулеметных очередей и пустил ракету");
+            Action(rifle, "Выстрелила с точным попаданием в уязвимое место");
+
+            Console.Read();
         }
     }
 }
