3f073b687f11f80a8ac2e1f41ac17c0de3b2dc15
Lizard_W1zard <artemef1996@gmail.com>
2020-04-11 22:51:58 +0300

    outPut

--- interface/Program.cs
+++ interface/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace HelloApp
 {
@@ -6,28 +7,62 @@ namespace HelloApp
     {
         void Shoot();
     }
+    interface IMovable
+    {
+        void Move();
+    }
+    interface IFlyable
+    {
+        public void Fly();
+    }
 
-    class Tank : IShooting
+
+
+    class Tank : IShooting, IMovable, IFlyable
     {
         public void Shoot()
         {
             Console.WriteLine("Выстрелил фугасом из орудия");
         }
+        public void Move()
+        {
+            Console.WriteLine("Проехал по грязи");
+        }
+        public void Fly()
+        {
+            Console.WriteLine("Не может летать");
+        }
     }
 
-    class Halicopter : IShooting
+    class Halicopter : IShooting, IMovable, IFlyable
     {
         public void Shoot()
         {
             Console.WriteLine("Дал несколько пулеметных очередей и пустил ракету");
         }
+        public void Move()
+        {
+            Console.WriteLine("Пролетел над посёлком");
+        }
+        public void Fly()
+        {
+            Console.WriteLine("Взлетел на 200 метров");
+        }
     }
 
-    class Rifle : IShooting
+    class Rifle : IShooting, IMovable, IFlyable
     {
         public void Shoot()
         {
             Console.WriteLine("Выстрелила с точным попаданием в уязвимое место");
+        }
+        public void Move()
+        {
+            Console.WriteLine("Не может перемещатся");
+        }
+        public void Fly()
+        {
+            Console.WriteLine("Не может летать");
         }
     }
 
@@ -40,9 +75,24 @@ namespace HelloApp
             Halicopter halicopter = new Halicopter();
             Rifle rifle = new Rifle();
 
-            tank.Shoot();
-            halicopter.Shoot();
-            rifle.Shoot();
+            List<IShooting> whoDoAction = new List<IShooting>();
+            whoDoAction.Add(tank);
+            whoDoAction.Add(halicopter);
+            whoDoAction.Add(rifle);
+
+            // что-то я в этом не уверен, наверное должно быть что-то проще
+            for(int i = 0; i < whoDoAction.Count; i++)
+            {
+                ((IFlyable)whoDoAction[i])?.Fly();
+            }
+            for (int i = 0; i < whoDoAction.Count; i++)
+            {                
+                ((IMovable)whoDoAction[i])?.Move();             
+            }
+            for (int i = 0; i < whoDoAction.Count; i++)
+            {
+                ((IShooting)whoDoAction[i])?.Shoot();
+            }
 
             Console.Read();
         }
